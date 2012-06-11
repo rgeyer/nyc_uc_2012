bef19ff7d3b3a4eda74417e94af0a84f39627cda
rgeyer <ryan.geyer@rightscale.com>
2012-06-11 19:39:43 +0000

    Add optional favorites

--- cookbooks/rjgeyer/metadata.rb
+++ cookbooks/rjgeyer/metadata.rb
@@ -14,3 +14,27 @@ attribute "about_me/first_name",
   :display_name => "First Name",
   :description => "Sufficiently described",
   :recipes => ["rjgeyer::default"]
+
+attribute "about_me/favorite/food",
+  :required => "optional",
+  :display_name => "Favorite food",
+  :description => "Your favorite food",
+  :category => "Favorites",
+  :default => "curry",
+  :recipes => ["rjgeyer::default"]
+
+attribute "about_me/favorite/drink",
+  :required => "optional",
+  :display_name => "Favorite drink",
+  :description => "Your favorite drink",
+  :category => "Favorites",
+  :default => "Jameson",
+  :recipes => ["rjgeyer::default"]
+
+attribute "about_me/favorite/hobby",
+  :required => "optional",
+  :display_name => "Favorite hobby",
+  :description => "Your Favorite hobby",
+  :category => "Favorites",
+  :default => "TV",
+  :recipes => ["rjgeyer::default"]
