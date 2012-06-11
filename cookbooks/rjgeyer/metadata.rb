5cedec6b4c375b7344224dcd28ab59bee2b72e44
rgeyer <ryan.geyer@rightscale.com>
2012-06-11 18:08:55 +0000

    Initial commit of rjgeyer cookbook

---
+++ cookbooks/rjgeyer/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Ryan J. Geyer"
+maintainer_email "ryan.geyer@rightscale.com"
+license          "All rights reserved"
+description      "Installs/Configures rjgeyer"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "rjgeyer::default","Prints Hello World! to the logs"
